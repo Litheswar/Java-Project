032a9568127fca8551b61dbb46a32d727db0748a
Litheswar <litheswar19@gmail.com>
2025-08-13 15:30:30 +0530

    updated Main file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        System.out.println("=== Welcome to Smart Travel Planner ===");
+        System.out.println("1. Plan a Trip");
+        System.out.println("2. View Saved Trips");
+        System.out.println("3. Exit");
+
+        // We will add menu handling logic later
+    }
 }
